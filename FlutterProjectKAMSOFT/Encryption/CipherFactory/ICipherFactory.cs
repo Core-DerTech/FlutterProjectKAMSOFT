54bfd897cbf1c958e342d4227d76cd2208ffb1b9
vampire7432804732985734985743948 <romannagik@gmail.com>
2026-05-21 03:31:55 -1100

    .

--- FlutterProjectKAMSOFT/Encryption/CipherFactory/ICipherFactory.cs
+++ FlutterProjectKAMSOFT/Encryption/CipherFactory/ICipherFactory.cs
@@ -1,6 +1,34 @@
-﻿namespace FlutterProjectKAMSOFT.Encryption.CipherFactory
+﻿using FlutterProjectKAMSOFT.Encryption.Ciphers;
+
+namespace FlutterProjectKAMSOFT.Encryption.CipherFactory
 {
-    public interface ICipherFactory
+    public class CipherFactory
     {
+        private readonly CaesarCipher _caesar;
+        private readonly VigenereCipher _vigenere;
+        private readonly RSAEncryption _rsa;
+        private readonly SHAEncription _sha;
+
+        public CipherFactory(
+            CaesarCipher caesar,
+            VigenereCipher vigenere,
+            RSAEncryption rsa,
+            SHAEncription sha)
+        {
+            _caesar = caesar;
+            _vigenere = vigenere;
+            _rsa = rsa;
+            _sha = sha;
+        }
+
+        public object CreateCipher(CipherType cipherType)
+            => cipherType switch
+            {
+                CipherType.Caesar => _caesar,
+                CipherType.Vigenere => _vigenere,
+                CipherType.RSAEncryption => _rsa,
+                CipherType.SHA1Encryption => _sha,
+                _ => throw new ArgumentException("No cipher was provided")
+            };
     }
 }
